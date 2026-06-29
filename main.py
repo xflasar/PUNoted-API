0f935ac6d3af3163fcd11fbe0cb367fe13462b74
Martin Flasar <christian.flasar@gmail.com>
2026-06-29 05:59:20 +0200

    fix: for firefox cors

--- main.py
+++ main.py
@@ -279,7 +279,7 @@ app.add_middleware(SecurityLoggerMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5174", "http://127.0.0.1:5174", "https://punoted.net"],
-    allow_origin_regex=r"chrome-extension://.*", 
+    allow_origin_regex=r"(chrome|moz)-extension://.*", 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
